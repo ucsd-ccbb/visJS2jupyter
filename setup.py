5c720fcdf427408825169286b45f4396171ced41
m1webste <m1webste@ucsd.edu>
2016-08-29 16:15:10 -0700

    This structure should work now! We can get rid of anything extra.

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
-from setuptools import setup, find_packages
+from distutils.core import setup
 
 setup(
-    name = "visJS-2-jupyter",
-    version = "0.3",
+    name = "visJS2jupyter",
+    packages = ["visJS2jupyter"],
+    version = "0.0.1",
     description= "visJS_2_jupyter is a tool to bring the interactivity of networks created with vis.js into jupyter notebook cells",
-    url = "http://visjs.org/docs/network/",
+    url = "https://github.com/ucsd-ccbb/visJS_2_jupyter/",
     author="Brin Rosenthal (sbrosenthal@ucsd.edu), Mikayla Webster (m1webste@ucsd.edu), Aaron Gary (agary@ucsd.edu)",
+    author_email="sbrosenthal@ucsd.edu",
+    keywords = ['jupyter notebook', 'interactive', 'network'],
     license = 'MIT',
     classifiers=[
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2',
-    ],
-    packages=find_packages()
+    ]
 )
