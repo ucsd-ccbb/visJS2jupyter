98b1861acb812690f8293c9e2d94690110665ade
Julia Len <jlen@ucsd.edu>
2017-04-20 10:31:52 -0700

    fixed bug in colocalization

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name = "visJS2jupyter",
     packages = ["visJS2jupyter"],
-    version = "0.1.5",
+    version = "0.1.6",
     description= "visJS2jupyter is a tool to bring the interactivity of networks created with vis.js into Jupyter notebook cells",
     url = "https://github.com/ucsd-ccbb/visJS2jupyter",
     author="Brin Rosenthal (sbrosenthal@ucsd.edu), Mikayla Webster (m1webste@ucsd.edu), Aaron Gary (agary@ucsd.edu), Julia Len (jlen@ucsd.edu)",
