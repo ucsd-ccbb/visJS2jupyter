67c6880630a793c12ef3ccfbf49b32ba8fb7d9cb
root <root@carlson-demo-gpu.c.speech-1518535569363.internal>
2018-11-05 23:04:20 +0000

    modify setup file for deps

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name = 'visJS2jupyter',
-	packages=['visJS2jupyter']
+	packages=find_packages(exclude=[]),
+	install_requires=['matplotlib','networkx','py2cytoscape']
+
 )
